7b51f2135c4df58836d947f43248f540382a816d
ariverapaniza <ariverapaniza@gmail.com>
2024-03-14 21:56:21 +1100

    Fixed Search for Actors and Directors, Created User Profile page with the Watchlist list on Profile

--- search.php
+++ search.php
@@ -7,10 +7,13 @@ $searchTerm = isset($_GET['query']) ? $_GET['query'] : '';
 
 $moviesSql = "SELECT * FROM movies WHERE title LIKE ?";
 $genreSql = "SELECT * FROM movies WHERE genre LIKE ?";
-$actorsfSql = "SELECT * FROM actors WHERE fname LIKE ?";
-$actorslSql = "SELECT * FROM actors WHERE lname LIKE ?";
-$directorsfSql = "SELECT * FROM directors WHERE fname LIKE ?";
-$directorslSql = "SELECT * FROM directors WHERE lname LIKE ?";
+//$actorsSearchSql = "SELECT * FROM actors WHERE id LIKE ?";
+// $actorsfSql = "SELECT * FROM actors WHERE fname LIKE ?";
+// $actorslSql = "SELECT * FROM actors WHERE lname LIKE ?";
+$actorsSql = "SELECT actorid, fname, lname, photo FROM actors WHERE CONCAT(fname, ' ', lname) LIKE ?";
+// $directorsfSql = "SELECT * FROM directors WHERE fname LIKE ?";
+// $directorslSql = "SELECT * FROM directors WHERE lname LIKE ?";
+$directorsSql = "SELECT directorid, fname, lname, photo FROM directors WHERE CONCAT(fname, ' ', lname) LIKE ?";
 
 $results = ['movies' => [], 'actors' => [], 'directors' => []];
 
@@ -30,31 +33,36 @@ function searchTable($conn, $sql, $searchTerm, &$results, $table)
 
 searchTable($conn, $moviesSql, $searchTerm, $results, 'movies');
 searchTable($conn, $genreSql, $searchTerm, $results, 'movies');
-searchTable($conn, $actorsfSql, $searchTerm, $results, 'actors');
-searchTable($conn, $actorslSql, $searchTerm, $results, 'actors');
-searchTable($conn, $directorsfSql, $searchTerm, $results, 'directors');
-searchTable($conn, $directorslSql, $searchTerm, $results, 'directors');
+//searchTable($conn, $actorsSearchSql, $searchTerm, $results, 'actors');
+// searchTable($conn, $actorsfSql, $searchTerm, $results, 'actors');
+// searchTable($conn, $actorslSql, $searchTerm, $results, 'actors');
+searchTable($conn, $actorsSql, $searchTerm, $results, 'actors');
+// searchTable($conn, $directorsfSql, $searchTerm, $results, 'directors');
+// searchTable($conn, $directorslSql, $searchTerm, $results, 'directors');
+searchTable($conn, $directorsSql, $searchTerm, $results, 'directors');
 
 // HTML to display results
 echo "<div class='container mt-5'>";
 echo "<h2>Search Results for '$searchTerm'</h2>";
 
 // Display movie results
-echo "<h3>Movies</h3>";
+echo "<h3>Movies</h3></br>";
 foreach ($results['movies'] as $movie) {
     echo "<H3><a href='movie_info.php?title=" . urlencode($movie['title']) . "'><img src='" . htmlspecialchars($movie['poster']) . "' alt='Movie Poster' style='width: 100px; height: auto;'><a href='movie_info.php?title=" . urlencode($movie['title']) . "'>" . htmlspecialchars($movie['title']) . "</a></H3>";
 }
 
 // Display Actor results
-echo "<h3>Actors</h3>";
+echo "</br><h3>Actors</h3></br>";
 foreach ($results['actors'] as $actor) {
-    echo "<H3><a href='actor_profile.php?fname=" . urlencode($actor['fname']) . "&lname=" . urlencode($actor['lname']) . "'>" . htmlspecialchars($actor['fname']) . " " . htmlspecialchars($actor['lname']) . "</a></H3>";
+    echo "<h3><a href='actor_profile.php?actorid=" . $actor['actorid'] . "'><img src='" . htmlspecialchars($actor['photo']) . "' alt='Actor Photo' style='width: 100px; height: auto;'><a href='actor_profile.php?actorid=" . $actor['actorid'] . "'>" . htmlspecialchars($actor['fname']) . " " . htmlspecialchars($actor['lname']) . "</a>
+</h3>";
 }
 
 // Display Director results
-echo "<h3>Directors</h3>";
+echo "</br><h3>Directors</h3></br>";
 foreach ($results['directors'] as $director) {
-    echo "<H3><a href='director_profile.php?fname=" . urlencode($director['fname']) . "&lname=" . urlencode($director['lname']) . "'>" . htmlspecialchars($director['fname']) . " " . htmlspecialchars($director['lname']) . "</a></H3>";
+    echo "<h3><a href='director_profile.php?directorid=" . $director['directorid'] . "'><img src='" . htmlspecialchars($director['photo']) . "' alt='Director Photo' style='width: 100px; height: auto;'><a href='director_profile.php?directorid=" . $director['directorid'] . "'>" . htmlspecialchars($director['fname']) . " " . htmlspecialchars($director['lname']) . "</a>
+</h3>";
 }
 
 echo "</div>";
